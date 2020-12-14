3158bd9de141b23c898bec95c463b0314230fbc1
Jed Brown <github@jedbrown.net>
2020-12-14 11:28:42 -0800

    First Commit

---
+++ Virtual Dew Point/VirtualDewPoint-driver.groovy
@@ -0,0 +1,33 @@
+// V0.02  Virtual Dew Point Device
+
+metadata {
+    definition (name: "Virtual DewPoint", namespace: "JohnRob", author: "several") {
+        capability  "Sensor"
+        command     "setDewPoint", ["NUMBER"]   // this will be a method.  [] may cause an input box to be created must test.
+        attribute   "DewPoint", "Number"      // this will go into the Hub database
+
+    }
+    preferences {       // These become entries in the "device" page to ask us for our preferences!
+        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+    }
+}
+
+def installed() {
+    log.warn "installed..."
+    setDewPoint(0)
+}
+
+def updated() {
+    log.info "updated..."
+    log.warn "description logging is: ${txtEnable == true}"
+}
+
+def parse(String description) {
+}       // basically useless.  Included because it is included in the template.
+
+def setDewPoint(dewpoint) {
+    //log.debug "   29  dewpoint =   ${dewpoint}"
+    def descriptionText = "${device.displayName} was set to $dewpoint"
+    if (txtEnable) log.info "${descriptionText}"
+    sendEvent(name: "DewPoint", value: dewpoint, unit: "°F", descriptionText: descriptionText)
+}
